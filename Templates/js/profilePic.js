610cfb42f784138011dafd0bc72c4da6d4ffe26e
Pavel <paveld@cybage.com>
2022-02-09 14:26:25 +0530

    Adding templates and removing unwanted files

---
+++ Templates/js/profilePic.js
@@ -0,0 +1,14 @@
+const image_input = document.querySelector('#fileTag');
+    var uploaded_image = "";
+
+    image_input.addEventListener("change",function(){
+        const reader = new FileReader();
+        reader.addEventListener("load", ()=>{
+           uploaded_image = reader.result
+           document.querySelector('#previewImg').style.backgroundImage = `url(${uploaded_image})`
+           document.querySelector('#previewImg2').style.backgroundImage = `url(${uploaded_image})`
+           document.querySelector('#previewImg3').style.backgroundImage = `url(${uploaded_image})`
+
+        })
+        reader.readAsDataURL(image_input.files[0])
+    })
